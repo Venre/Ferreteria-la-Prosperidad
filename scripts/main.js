9b87a60e5dc23031a0fc923c004fba73c9ee0df7
David <davidgmb1818@gmail.com>
2024-11-20 02:38:27 -0500

    Cambios en la estructura General y realizacion de Secciones de login y Register

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Register
+    const registerForm = document.getElementById('register-form');
+    if (registerForm) {
+        registerForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const nombre = document.getElementById('usuario-nombre').value;
+            const apellido = document.getElementById('usuario-apellido').value;
+            const email = document.getElementById('usuario-email').value;
+            const password = document.getElementById('contraseña').value;
+            const genero = document.getElementById('genero').value;
+
+            const user = {
+                nombre: nombre,
+                apellido: apellido,
+                email: email,
+                password: password,
+                genero: genero
+            };
+
+            localStorage.setItem(email, JSON.stringify(user));
+            alert('Registro exitoso. Ahora puedes iniciar sesión.');
+            window.location.href = 'login.html'; // Redirige a la página de login
+        });
+    } else {
+        console.error('Register form not found');
+    }
+
+    // Login form
+    const loginForm = document.getElementById('login-form');
+    if (loginForm) {
+        loginForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+
+            const email = document.getElementById('usuario-email-login').value;
+            const password = document.getElementById('contraseña-login').value;
+
+            console.log("Email ingresado:", email);
+            console.log("Contraseña ingresada:", password);
+
+            const storedUser = localStorage.getItem(email);
+            if (storedUser) {
+                const userData = JSON.parse(storedUser);
+                console.log("Usuario almacenado:", userData);
+
+                if (userData.password.trim() === password.trim()) {
+                    alert('Inicio de sesión exitoso.');
+                    window.location.href = 'index.html';
+                } else {
+                    alert('Contraseña incorrecta.');
+                }
+            } else {
+                alert('Usuario no encontrado.');
+            }
+        });
+    } else {
+        console.error('Login form not found');
+    }
+});
